96b0bd69cc993a2397056387ff4dbc2317bbff32
Gabriel Aniora <chybesta@gmail.com>
2019-02-10 02:05:53 +0100

    add candidate.js

---
+++ src/Components/Candidates.js
@@ -0,0 +1,128 @@
+module.exports = [
+  { 
+    id: 1,
+    fullName: 'FRANCIS .C. CONFIDENCE', 
+    regNo: 'CCT/AB31/08/18',
+    programType: 'REGULAR'
+  },
+  { 
+    id: 2,
+    fullName: 'OKEZIE .O. FAVOUR',
+    regNo: 'CCT/AB12/05/18', 
+    programType: 'REGULAR'
+  },
+  {
+    id: 3,
+    fullName: 'SILAS .C. WINNER',
+    regNo: 'CCT/AB26/08/18',
+    programType: 'REGULAR'
+  }, 
+  {
+    id: 4,
+    fullName: 'IBE DANIEL OBASI',
+    regNo: 'CCT/AB27/08/18',
+    programType: 'REGULAR'
+  },
+  {
+    id: 5,
+    fullName: 'JOY .C. CHUKWUEMEKA',
+    regNo: 'CCT/AB20/07/18',
+    programType: 'EXECUTIVE'
+  },
+  {
+    id: 6,
+    fullName: 'DAVID SILAS',
+    regNo: 'CCT/AB28/08/18',
+    programType: 'REGULAR'
+  },
+  {
+    id: 7,
+    fullName: 'UKO AKACHUKWU GLORY',
+    regNo: 'CCT/AB23/07/18',
+    programType: 'REGULAR'
+  },
+  {
+    id: 8,
+    fullName: 'EMMANUEL .S. BERRY',
+    regNo: 'CCT/AB32/08/18',
+    programType: 'REGULAR'
+  },
+  {
+    id: 9,
+    fullName: 'UKOHA KINGSELY KALU',
+    regNo: 'CCT/AB33/08/18',
+    programType: 'REGULAR'
+  },
+  {
+    id: 10,
+    fullName: 'UKO OLUOMACHI',
+    regNo: 'CCT/AB45/08/16',
+    programType: 'REGULAR'
+  },
+  {
+    id: 11,
+    fullName: 'UKOHA CHIMERE DANIEL',
+    regNo: 'CCT/AB30/08/18',
+    programType: 'REGULAR'
+  },
+  {
+    id: 12,
+    fullName: 'EMMANUEL MIRIAN AMARACHI',
+    regNo: 'CCT/AB11/05/18',
+    programType: 'REGULAR'
+  },
+  {
+    id: 13,
+    fullName: 'AKPAN PHENSON MARYANN VIVAN',
+    regNo: 'CCT/AB10/05/18',
+    programType: 'REGULAR'
+  },
+  {
+    id: 14,
+    fullName: 'DANIEL EMMANUEL OLUEBUBE',
+    regNo: 'CCT/AB25/08/18',
+    programType: 'REGULAR'
+  },
+  {
+    id: 15,
+    fullName: 'UKOHA DAVID CHISOM',
+    regNo: 'CCT/AB29/08/18',
+    programType: 'REGULAR'
+  },
+  {
+    id: 16,
+    fullName: 'ANOKE DIVINE FAVOUR',
+    regNo: 'CCT/AB19/06/18',
+    programType: 'REGULAR'
+  },
+  {
+    id: 17,
+    fullName: 'SILAS SUNDAY NNAMDI',
+    regNo: 'CCT/AB02/01/18',
+    programType: 'REGULAR'
+  },
+  {
+    id: 18,
+    fullName: 'UGOCHUKWU ESTHER CHINONYEREM',
+    regNo: 'CCT/AB17/07/18',
+    programType: 'REGULAR'
+  },
+  {
+    id: 19,
+    fullName: 'AMOGU SUNNY NDUKWE',
+    regNo: 'CCT/AB22/07/18',
+    programType: 'REGULAR'
+  },
+  {
+    id: 20,
+    fullName: 'OBASI NKECHI GODWIN',
+    regNo: 'CCT/AB14/06/18',
+    programType: 'REGULAR'
+  },
+  {
+    id: 21,
+    fullName: 'EMMANUEL GODSON',
+    regNo: 'CCT/AB24/08/18',
+    programType: 'REGULAR'
+  }
+]
