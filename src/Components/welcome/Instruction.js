a192c6bfd3e6c886e65fff30305f1c73afb3fd61
Gabriel Aniora <chybesta@gmail.com>
2019-07-11 04:26:25 +0100

    update text

--- src/Components/welcome/Instruction.js
+++ src/Components/welcome/Instruction.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import image from '../images/instructor.png';
+import image from '../images/instructor.jpg';
 import Typing from 'react-typing-animation';
 
 
